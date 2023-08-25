76f7a98ae272f49dffaf45abd04f6900e852fa7d
dmitry_nasyrkulov <dmitry.nasyrkulov@digitickets.co.uk>
2023-08-25 16:12:51 +0300

    Task requests done

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('tasks', TaskController::class)->only([
-    'index',
-    'show',
-    'store',
-]);
+Route::apiResource('tasks', TaskController::class);
